e66925e45beed9e24bf5bd1967579bf93d81d821
kareem <kareem.janou@gmail.com>
2021-09-13 22:44:34 +0200

    Dataloader and visualization

--- dataset_utils/celeba_utils.py
+++ dataset_utils/celeba_utils.py
@@ -1,5 +1,58 @@
 import os
 import shutil
+from collections import defaultdict
+from pathlib import Path
+
+split_map = None
+def get_split_map(split_txt_path):
+    global split_map
+
+    if split_map is None:
+        split_map = defaultdict(list)
+
+        with open(split_txt_path) as file:
+            split_data = file.readlines()
+
+        for line in split_data:
+            split_line = line.split()
+            split = int(split_line[1])
+            if split == 0:
+                split = "train"
+            elif split == 1:
+                split = "val"
+            elif split == 2:
+                split = "test"
+            image_name = split_line[0]
+            split_map[split].append(image_name)
+    return split_map
+
+split_labels_map = None
+def get_split_labels_map(label_txt_path, split_txt_path):
+    global split_labels_map
+    if split_labels_map is None:
+        split_labels_map = {}
+        for split in ['train', 'val', 'test']:
+            split_labels_map[split] = defaultdict(list)
+
+        split_map = get_split_map(split_txt_path)
+        assert Path(label_txt_path).exists(), "label_txt_path doesn't exist!!!"
+        with open(label_txt_path) as file:
+            label_data = file.readlines()
+
+        for line in label_data:
+            split_line = line.split()
+            label = int(split_line[1])
+            image_name = split_line[0]
+            image_split = "train"
+
+            for split, image_list in list(split_map.items())[1:]:
+                if image_name in image_list:
+                    image_split = split
+
+            split_labels_map[image_split][label].append(image_name)
+
+    return split_labels_map
+
 
 def celeba_split_from_file(data_path, split_txt_path):
     """
@@ -15,26 +68,9 @@ def celeba_split_from_file(data_path, split_txt_path):
     :return: dict of lists containing image file names corresponding to each partition.
     keys are "train", "val", and "test"
     """
-    map = {'train': [],
-           'val': [],
-           'test': []}
-
     print('\nSplitting images to train, val, and test partitions...')
-
-    with open(split_txt_path) as file:
-        data = file.readlines()
-
-    for line in data:
-        split_line = line.split()
-        split = int(split_line[1])
-        if split == 0:
-            split = "train"
-        elif split == 1:
-            split = "val"
-        elif split == 2:
-            split = "test"
-        image_name = split_line[0]
-        map[split].append(image_name)
+    split_map = get_split_map(split_txt_path)
+    for split, image_name in list(split_map.items()):
         src_path = os.path.join(data_path, image_name)
 
         if not os.path.exists(src_path):
@@ -50,7 +86,7 @@ def celeba_split_from_file(data_path, split_txt_path):
         shutil.copyfile(src_path, img_dst_path)
         os.remove(src_path)
     print("Splitting completed.\n")
-    return map
+    return split_map
 
 
 def celeba_save_images_in_folders(data_path, label_txt_path, map=None):
---
+++ image_utils/visualization_utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage import io, color
+
+def visualize_samples(imgs_path, gray=False, n_cols=5, n_rows=1):
+    """Visualize samples."""
+
+    plt.figure(figsize=(3*n_cols, 3*n_rows))
+    for n,i in enumerate(np.random.randint(len(imgs_path), size=n_cols*n_rows)):
+        plt.subplot(n_rows,n_cols, n+1)
+        plt.axis('off')
+        img = io.imread(imgs_path[i])
+        if gray:
+            img = color.rgb2gray(img)
+            plt.imshow(img, cmap=plt.cm.gray)
+        else:
+            plt.imshow(img)
+    plt.show()
+
+def visualize_torch_batch(images, gray=False, n_cols=5, n_rows=1, caption=""):
+    """Visualize a batch of images of type torch tensor"""
+
+    fig = plt.figure(figsize = (3*n_cols,3*n_rows))
+    for i in range(n_cols*n_rows):
+        plt.subplot(n_rows,n_cols,i+1)
+        plt.axis('off')
+        img = images[i].permute(1, 2, 0).squeeze()
+        if gray:
+            plt.imshow(img, cmap=plt.cm.gray)
+        else:
+            plt.imshow(img)
+
+    fig.text(0, 0, caption)
+    return fig
