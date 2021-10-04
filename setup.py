de4311d29f3dd0e9284145cab1ef70fcdf993588
kareem <kareem.janou@gmail.com>
2021-10-04 20:44:20 +0200

    updateing reqs and setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", 'r') as file:
+    libs = file.readlines()
+
+setup(
+    name = "image_toolbox",
+    version = "0.0.1",
+    author = "Kareem Janou",
+    description = ("Collection of usefull tools, that can make the developement of neural networks easier."),
+    license = "BSD",
+    keywords = "ML tools AI CNN pytorch",
+    packages=find_packages(),
+    install_requires= libs,
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
