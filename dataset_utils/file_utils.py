09b5ffa04ab49df7b98d2b5c3618ff13b85af86f
kareem <kareem.janou@gmail.com>
2021-11-01 23:13:34 +0100

    refactoring

--- dataset_utils/file_utils.py
+++ dataset_utils/file_utils.py
@@ -10,7 +10,6 @@ def unzip_file(zip_path, dest_path):
     zip_file = zipfile.ZipFile(zip_path, "r")
     zip_file.extractall(path=dest_path)
     zip_file.close()
-    # os.remove(zip_path)
     imgs_path = Path(dest_path) / Path(zip_path).stem
     print("Image are in", str(imgs_path))
     return imgs_path
